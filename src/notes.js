643efe10dedea7a1dd9e508d38ffe67f94797063
pedros18 <badrofcb2004@gmail.com>
2024-03-26 10:54:27 +0100

    this is sticky notes

---
+++ src/notes.js
@@ -0,0 +1,26 @@
+const notes = [
+  {
+    key: 1,
+    title: "react",
+    content:
+      "its the most known technology for frontend devlopment"
+  },
+  {
+    key: 2,
+    title: "backend",
+    content:
+      "how your application is gonna work behind the scenses"
+  },
+  {
+    key: 3,
+    title: "design",
+    content:
+      "every full stack devlopper is normally a great designer"
+  },
+  {
+    key: 4,
+    title: "Hardware vs. Software",
+    content:
+      "hardware is physical but software is everything about logiciel"  }
+];
+export default notes;
